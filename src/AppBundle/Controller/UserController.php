60b8c2dead1907143743a1b71095196c7eb8d0e4
marcosjrs <marcosjrs80@gmail.com>
2018-09-10 19:29:45 +0200

    Creacion Formulario, Pagina y Ruta de Registro (sin funcionalidad final)

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -4,11 +4,25 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use BackendBundle\Entity\User;
+use \AppBundle\Form\RegisterType;
 
 class UserController extends Controller
 {
     public function loginAction(\Symfony\Component\HttpFoundation\Request $req)
     {
         return $this->render('@App/User/login.html.twig');
+    }
+    
+    public function registerAction(\Symfony\Component\HttpFoundation\Request $req)
+    {
+        $user = new User();        
+        
+        $form = $this->createForm(RegisterType::class, $user);
+       
+        
+        return $this->render('@App/User/register.html.twig', array(
+            "form" => $form->createView()
+        ));
     }
 }
